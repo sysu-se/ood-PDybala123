ff41e23bc960ba407a52af5d6ec332acb034e12a
PDybala123 <18526620672@163.com>
2026-05-05 17:48:42 +0800

    Update index.svelte

--- src/components/Board/index.svelte
+++ src/components/Board/index.svelte
@@ -1,31 +1,10 @@
 <script>
-	import { BOX_SIZE } from '@sudoku/constants';
-	import { gamePaused } from '@sudoku/stores/game';
-	import { grid, userGrid, invalidCells } from '@sudoku/stores/grid';
-	import { settings } from '@sudoku/stores/settings';
-	import { cursor } from '@sudoku/stores/cursor';
-	import { candidates } from '@sudoku/stores/candidates';
-	import Cell from './Cell.svelte';
+	import Cell from './Cell.svelte'
+	import { game } from '../../../App.svelte'
+	import { selected } from '../../stores/selected.js'
 
-	function isSelected(cursorStore, x, y) {
-		return cursorStore.x === x && cursorStore.y === y;
-	}
-
-	function isSameArea(cursorStore, x, y) {
-		if (cursorStore.x === null && cursorStore.y === null) return false;
-		if (cursorStore.x === x || cursorStore.y === y) return true;
-
-		const cursorBoxX = Math.floor(cursorStore.x / BOX_SIZE);
-		const cursorBoxY = Math.floor(cursorStore.y / BOX_SIZE);
-		const cellBoxX = Math.floor(x / BOX_SIZE);
-		const cellBoxY = Math.floor(y / BOX_SIZE);
-		return (cursorBoxX === cellBoxX && cursorBoxY === cellBoxY);
-	}
-
-	function getValueAtCursor(gridStore, cursorStore) {
-		if (cursorStore.x === null && cursorStore.y === null) return null;
-
-		return gridStore[cursorStore.y][cursorStore.x];
+	function handleClick(x, y) {
+		selected.set(y, x) // 注意 row=y, col=x
 	}
 </script>
 
@@ -33,27 +12,25 @@
 	<div class="max-w-xl relative">
 		<div class="w-full" style="padding-top: 100%"></div>
 	</div>
+
 	<div class="board-padding absolute inset-0 flex justify-center">
+		<div class="bg-white shadow-2xl rounded-xl overflow-hidden w-full h-full max-w-xl grid">
 
-		<div class="bg-white shadow-2xl rounded-xl overflow-hidden w-full h-full max-w-xl grid" class:bg-gray-200={$gamePaused}>
-
-			{#each $userGrid as row, y}
+			{#each $game.grid as row, y}
 				{#each row as value, x}
-					<Cell {value}
-					      cellY={y + 1}
-					      cellX={x + 1}
-					      candidates={$candidates[x + ',' + y]}
-					      disabled={$gamePaused}
-					      selected={isSelected($cursor, x, y)}
-					      userNumber={$grid[y][x] === 0}
-					      sameArea={$settings.highlightCells && !isSelected($cursor, x, y) && isSameArea($cursor, x, y)}
-					      sameNumber={$settings.highlightSame && value && !isSelected($cursor, x, y) && getValueAtCursor($userGrid, $cursor) === value}
-					      conflictingNumber={$settings.highlightConflicting && $grid[y][x] === 0 && $invalidCells.includes(x + ',' + y)} />
+					<div on:click={() => handleClick(x, y)}>
+						<Cell
+							value={value}
+							cellY={y + 1}
+							cellX={x + 1}
+							selected={$selected.row === y && $selected.col === x}
+							userNumber={true}
+						/>
+					</div>
 				{/each}
 			{/each}
 
 		</div>
-
 	</div>
 </div>
 
