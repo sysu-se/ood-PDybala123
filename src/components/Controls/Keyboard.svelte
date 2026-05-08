a7bef7471fa8aa5aac512e784a0eb8e3b264efda
PDybala123 <18526620672@163.com>
2026-05-08 15:00:28 +0800

    Update Keyboard.svelte

--- src/components/Controls/Keyboard.svelte
+++ src/components/Controls/Keyboard.svelte
@@ -1,48 +1,102 @@
 <script>
-	import { game } from '../../App.svelte'
-	import { selected } from '../../stores/selected.js'
+	import { createEventDispatcher } from 'svelte';
+	import { cursor } from '../../domain/stores/cursor.js';
+	import { notes } from '../../domain/stores/notes.js';
+	import { keyboardDisabled } from '../../domain/stores/keyboard.js';
+
+	export let onAction = () => {};
 
 	function handleKeyButton(num) {
-		if ($selected.row === null) return
+		if (!$keyboardDisabled) {
+			const row = $cursor.y;
+			const col = $cursor.x;
 
-		game.guess({
-			row: $selected.row,
-			col: $selected.col,
-			value: num === 0 ? 0 : num
-		})
+			const value = num === 0 ? null : num;
+
+			onAction('guess', { row, col, value });
+		}
 	}
 
 	function handleKey(e) {
-		if (e.key >= '1' && e.key <= '9') {
-			handleKeyButton(Number(e.key))
-		}
 
-		if (e.key === 'Backspace' || e.key === 'Delete') {
-			handleKeyButton(0)
-		}
-	}
+        if ((e.ctrlKey || e.metaKey) && e.key.toLowerCase() === 'z') {
+            e.preventDefault();
+            if (e.shiftKey) {
+                onAction('redo');
+            } else {
+                onAction('undo');
+            }
+            return;
+        }
+
+        switch (e.key || e.keyCode) {
+            case 'ArrowUp':
+            case 38:
+            case 'w':
+            case 87:
+                cursor.move(0, -1);
+                break;
+            case 'ArrowDown':
+            case 40:
+            case 's':
+            case 83:
+                cursor.move(0, 1);
+                break;
+            case 'ArrowLeft':
+            case 37:
+            case 'a':
+            case 65:
+                cursor.move(-1);
+                break;
+            case 'ArrowRight':
+            case 39:
+            case 'd':
+            case 68:
+                cursor.move(1);
+                break;
+            case 'Backspace':
+            case 8:
+            case 'Delete':
+            case 46:
+                handleKeyButton(0); // 触发删除
+                break;
+            default:
+                if (e.key && e.key * 1 >= 0 && e.key * 1 < 10) {
+                    handleKeyButton(e.key * 1);
+                } else if (e.keyCode - 48 >= 0 && e.keyCode - 48 < 10) {
+                    handleKeyButton(e.keyCode - 48);
+                }
+                break;
+        }
+    }
 </script>
 
-<svelte:window on:keydown={handleKey} />
+<svelte:window on:keydown={handleKey} /><!--on:beforeunload|preventDefault={e => e.returnValue = ''} />-->
 
 <div class="keyboard-grid">
+
 	{#each Array(10) as _, keyNum}
 		{#if keyNum === 9}
-			<button class="btn btn-key" on:click={() => handleKeyButton(0)}>
-				Clear
+			<button class="btn btn-key" disabled={false} title="Erase Field" on:click={() => handleKeyButton(0)}>
+				<svg class="icon-outline" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+					<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+				</svg>
 			</button>
 		{:else}
-			<button class="btn btn-key" on:click={() => handleKeyButton(keyNum + 1)}>
+			<button class="btn btn-key" disabled={false} title="Insert {keyNum + 1}" on:click={() => handleKeyButton(keyNum + 1)}>
 				{keyNum + 1}
 			</button>
 		{/if}
 	{/each}
+
 </div>
 
 <style>
 	.keyboard-grid {
 		@apply grid grid-rows-2 grid-cols-5 gap-3;
 	}
+
+
 	.btn-key {
 		@apply py-4 px-0;
 	}
