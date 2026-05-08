14501e1f40996c431411d274f3056f874b47c4b4
PDybala123 <18526620672@163.com>
2026-05-08 15:00:53 +0800

    Pass props to ActionBar and Keyboard components

--- src/components/Controls/index.svelte
+++ src/components/Controls/index.svelte
@@ -1,12 +1,16 @@
 <script>
 	import ActionBar from './ActionBar/index.svelte';
 	import Keyboard from './Keyboard.svelte';
+
+	export let isExploring = false;
+	export let hintsRemaining = Infinity;
+	export let onAction = () => {};
 </script>
 
 <div class="px-4 pb-5 flex justify-center">
 	<div class="w-full max-w-xl">
-		<ActionBar />
+		<ActionBar {hintsRemaining} {isExploring} {onAction} />
 
-		<Keyboard />
+		<Keyboard {onAction} />
 	</div>
 </div>
