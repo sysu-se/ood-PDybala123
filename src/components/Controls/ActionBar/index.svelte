25aa44ad0bd75f8633bc60b8046fc5d20c5492cb
PDybala123 <18526620672@163.com>
2026-05-08 15:02:13 +0800

    Update index.svelte

--- src/components/Controls/ActionBar/index.svelte
+++ src/components/Controls/ActionBar/index.svelte
@@ -1,12 +1,16 @@
 <script>
 	import Timer from './Timer.svelte';
 	import Actions from './Actions.svelte';
+
+	export let onAction = () => {};
+	export let hintsRemaining = Infinity;
+	export let isExploring = false;
 </script>
 
 <div class="action-bar space-y-3 xs:space-y-0">
 	<Timer />
 
-	<Actions />
+	<Actions {onAction} {hintsRemaining} {isExploring} />
 </div>
 
 <style>
